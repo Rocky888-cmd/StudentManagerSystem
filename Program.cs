5a12657cec63e1d89e5970095a7a281a5d247da3
Rocky Malobago <rockymalobago080@gmail.com>
2025-09-17 21:19:51 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Rocky";
+int age = 29;
+string from = "San Jose";
+Console.WriteLine($"Hello I'm {name}, I'm {age}, I'm from {from}");
